d8e88076651c62f0343c50c8853ffa900f413aad
TanmaySahu <tanmaysahu839926@gmail.com>
2020-12-31 11:42:59 +0530

    Tanmay Sahu

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Footer() {
     return (
         <div className="footer_position">
-            <Link to="/upload"><h4 className="footer_text">Want to Earn? Upload Question Papers. Click here</h4></Link>
+            <Link to="/upload"><h4 className="footer_text">Want to Earn? Upload Question Papers. Click here!</h4></Link>
         </div>
     )
 }
